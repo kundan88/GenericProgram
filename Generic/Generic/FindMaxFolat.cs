c6261973ad5ebd01b93ce4a4b053d1717560968e
Kundan <kundan kamble>
2022-04-05 17:05:27 -0700

    [KUNDAN] Added Find Max Float Program

---
+++ Generic/Generic/FindMaxFolat.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic
+{
+    internal class FindMaxFolat
+    {
+        public static float MaximumFloatNumber(double firstValue, double secondValue, double thirdValue)
+        {
+            if (firstValue.CompareTo(secondValue) > 0 && secondValue.CompareTo(thirdValue) > 0)
+            {
+                return (float)firstValue;
+            }
+
+            if (secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) > 0)
+            {
+                return (float)secondValue;
+            }
+
+            if (thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(secondValue) > 0)
+            {
+                return (float)thirdValue;
+            }
+            throw new Exception("firstValue,secondValue,thirdValue are same");
+        }
+
+    }
+}
--- Generic/Generic/Program.cs
+++ Generic/Generic/Program.cs
@@ -3,3 +3,8 @@
 Console.WriteLine("find Max number");
 int output = FindMaxInteger.MaximumIntegerNumber(11, 22, 33);
 Console.WriteLine(output);
+
+Console.WriteLine("find Max number");
+
+double output1 = FindMaxFolat.MaximumFloatNumber(11.2, 22.6, 33.5);
+Console.WriteLine(output1);
